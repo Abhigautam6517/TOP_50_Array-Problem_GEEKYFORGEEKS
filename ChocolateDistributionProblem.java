40ff1104d7d7bbcecb2c2401d72e5dceea06a6f4
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-09-02 12:10:28 +0530

    Add files via upload

---
+++ ChocolateDistributionProblem.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class ChocolateDistributionProblem {
+    public long findMinDiff (ArrayList<Integer> a, int n, int m)
+    {
+        // your code here
+        Collections.sort(a);
+        long minDiff = Long.MAX_VALUE;
+        for(int i = 0;i+m-1<n;i++){
+            minDiff = Math.min(a.get(i+(int)m-1)-a.get(i),minDiff);
+        }
+         return minDiff;
+        
+    }
+   
+   
+    
+}
